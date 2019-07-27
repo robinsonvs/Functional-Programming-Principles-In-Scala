6a991e5cfd34bc9b4d4bb39ad2a34deccbce6523
Robinson Severo <robinsonvs@gmail.com>
2019-07-27 15:19:29 -0300

    add exercise week02 lecture2.7

---
+++ exercises/week02/exercise06Lecture27EvaluationAndOperators.scala
@@ -0,0 +1,26 @@
+package scala_exercises.week02
+
+//Provide a fully parenthized version of
+// a + b ^? c ^? d less a ==> b | c
+
+// precedence
+// (all letters)
+// |
+// ^
+// &
+// <>
+// = !
+// :
+// + -
+// * / %
+// (all other special characters)
+
+//
+
+object exercise06Lecture27EvaluationAndOperators extends App {
+
+  // (( a + b ) ^? ( c ^? d ))  less (( a ==> b ) | c )
+
+  // Every binary operation needs to be put into parentheses, but the
+  // structure of the expression shold not change
+}
