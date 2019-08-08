7ea5380cb9917d67fe4d2f1a13226cfb8768bf87
Robinson Severo <robinsonvs@gmail.com>
2019-08-08 09:07:58 -0300

    add exercise 04 week04

---
+++ exercises/week04/exercise04Lecture44Variance.scala
@@ -0,0 +1,19 @@
+package scala_exercises.week04
+
+// Say you have two function types
+//
+//    type A = IntSet => NonEmpty
+//    type B = NonEmpty => IntSet
+//
+// According to the Liskov Substitution Principle, which of the
+// following should be true ?
+//
+// 0 - A <: B
+// 0 - B >: A
+// 0 - A and B are unrelated
+//
+// R - A <: B
+
+object exercise04Lecture44Variance {
+
+}
